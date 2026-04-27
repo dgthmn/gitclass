3df45e19d4793b69ace482ddc553b44f9a223a5d
dgthmn <douglas.thierry85@gmail.com>
2026-04-27 12:15:10 -0300

    Adicionei o início do meu SEM - ME

--- aula.R
+++ aula.R
@@ -1,2 +1,20 @@
-#aula
+#aula - salvando meu escript
 
+# ============================================================
+# SCRIPT 3 – MODELOS DE EQUAÇÕES ESTRUTURAIS (SEM) – PIECEWISE
+# Autor: Douglas TM Neves
+# Objetivo: Rodar SEMs para diversidade de plantas e formigas, além de sobreposição de nicho.
+# Entradas: dataset_SEM.xlsx
+# Saídas: SEM_results_all_models.xlsx, SEM_GlobalFit_R2.xlsx
+# Pacotes necessários: readxl, stats, piecewiseSEM, dplyr, writexl
+# Observações: 
+#   - O dataset deve estar no diretório "../data/" relativo a este script
+#   - Todas as métricas de rede já estão padronizadas (z-score) devido ao script 1
+# ============================================================
+
+# ---------- Pacotes ----------
+library(readxl)
+library(stats)        # lm(), scale() — já vem no R base
+library(piecewiseSEM)
+library(dplyr)
+library(writexl)
